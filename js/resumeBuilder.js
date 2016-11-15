31c0ef977635a62e6991180093fec7948858edd8
Aleksandra Kirievskaya <Aleksandra11@users.noreply.github.com>
2016-11-15 11:09:03 -0600

    Update resumeBuilder.js with current info

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,6 +1,6 @@
 var bio = {
-	"name": "Aleksandra Kiri",
-	"role": "Front-End Developer",
+	"name": "Aleksandra Kirievskaya",
+	"role": "Front-End Web Developer",
 	"contacts": {
 		"email": "aleksakir1@gmail.com",
 		"github": "Aleksandra11",
@@ -9,12 +9,12 @@ var bio = {
 	},
 	"bioPic": "images/me_1.jpg",
 	"message": "Welcome to my interactive Resume",
-	"skills": ["HTML","CSS","JavaScript","Responsive Web Design", "Version Control"],
+	"skills": ["HTML, CSS","JavaScript", "Angular", "Backbone", "Knockout", "Jasmine", "Responsive Web Design", "Version Control"],
 	"display": function() {
 		var formettedName = HTMLheaderName.replace("%data%", bio.name);
 		var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 		$("#fixedname").prepend(formattedRole);
-		$("#fixedname").prepend(formettedName);
+		$("#fixedname").append(formettedName);
 
 		var bioPic = bio.bioPic;
 		var formattedPic = HTMLbioPic.replace("%data%", bioPic);
@@ -51,6 +51,14 @@ var bio = {
 			$("#skills").append(formattedSkill);
 			var formattedSkill = HTMLskills.replace("%data%",skills[3]);
 			$("#skills").append(formattedSkill);
+			var formattedSkill = HTMLskills.replace("%data%",skills[4]);
+			$("#skills").append(formattedSkill);
+			var formattedSkill = HTMLskills.replace("%data%",skills[5]);
+			$("#skills").append(formattedSkill);
+			var formattedSkill = HTMLskills.replace("%data%",skills[6]);
+			$("#skills").append(formattedSkill);
+			var formattedSkill = HTMLskills.replace("%data%",skills[7]);
+			$("#skills").append(formattedSkill);
 		}
 	}
 };
@@ -73,20 +81,47 @@ var education = {
 			"major": "Mathematics",
 			"dates": 2010,
 			"url": "http://en.psu.ru/"
-		}
-		],
-	"onlineClasses": [
-		{
-			"title": "Front-End Developer ND",
-			"school": "Udacity",
-			"date": 2015,
-			"url": "http://www.udacity.com/course/ud804"
 		},
 		{
-			"title": ["JavaScript Syntax","HTML and CSS"],
+			"name": "Front-End Web Developer Nanodegree",
+			"location": "Udacity",
+			"degree": " ",
+			"major": "Front-End Web Dev",
+			"dates": 2016,
+			"url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001?v=fe1"
+		}
+	],
+	"onlineClasses": [
+		{
+			"title": "Front-End Web Developer Nanodegree",
+			"school": "Udacity",
+			"date": 2016,
+			"url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001?v=fe1"
+		},
+		{
+			"title": "JavaScript Promises",
+			"school": "Udacity",
+			"date": 2016,
+			"url": "https://www.udacity.com/course/javascript-promises--ud898"
+		},
+		
+		{
+			"title": "Offline-web-applications",
+			"school": "Udacity",
+			"date": 2016,
+			"url": "https://www.udacity.com/course/offline-web-applications--ud899"
+		},
+		{
+			"title": "Building High Conversion Web Forms",
 			"school": "Udacity",
 			"date": 2015,
-			"url": "http://www.udacity.com/course/ud804"
+			"url": "https://www.udacity.com/course/building-high-conversion-web-forms--ud890"
+		},
+		{
+			"title": "Browser Rendering Optimization",
+			"school": "Udacity",
+			"date": 2015,
+			"url": "https://www.udacity.com/course/browser-rendering-optimization--ud860"
 		}
 	],
 	"display": function() {
@@ -94,7 +129,14 @@ var education = {
 		for(school in education.schools) {
 			$("#education").append(HTMLschoolStart);
 			var formattedName = HTMLschoolName.replace("%data%", education.schools[school].name);
-			$(".education-entry:last").append(formattedName);
+
+			var formattedSchoolUrl = HTMLschoolUrl.replace("%data%", education.schools[school].url);
+			var formattedFullName = formattedSchoolUrl + formattedName;
+
+			$(".education-entry:last").append(formattedFullName);
+
+
+
 			var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
 			$(".education-entry:last").append(formattedLocation);
 			var formattedDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
@@ -110,22 +152,29 @@ var education = {
 			$(".online-entry:last").append(formattedTitleSchool);
 			var formattedDate = HTMLonlineDates.replace("%data%", education.onlineClasses[onlineClass].date);
 			$(".online-entry:last").append(formattedDate);
+			//var formattedURL = HTMLonlineURL.replace("%data%", education.onlineClasses[onlineClass].url);
+			//$(".online-entry:last").append(formattedURL);
+			//var formattedNameUrl = HTMLonlineNameUrl.replace("%data%", education.onlineClasses[onlineClass].url);
+			//$(".online-entry:last").append(formattedNameUrl
 			var formattedURL = HTMLonlineURL.replace("%data%", education.onlineClasses[onlineClass].url);
-			$(".online-entry:last").append(formattedURL);
-		}
+			
+			var formattedNameUrl = HTMLonlineNameUrl.replace("%data%", education.onlineClasses[onlineClass].url);
+			var formattedOnlineUrl = formattedURL + formattedNameUrl;
+			$(".online-entry:last").append(formattedOnlineUrl);
+			}
 	}
 };
 education.display();
 
 var work = {
 	"jobs": [
-		{
-			"employer": "Planet Express",
-			"title": "Front-End Web Developer",
-			"location": "Richmond, VA",
-			"years": "2015",
-			"description": "Responsible for interpreting and executing designs correctly."
-		},
+		//{
+		//	"employer": "Planet Express",
+		//	"title": "Front-End Web Developer",
+		//	"location": "Richmond, VA",
+		//	"years": "2015",
+		//	"description": "Responsible for interpreting and executing designs correctly."
+		//},
 		{
 			"employer": "School for children with special needs",
 			"title": "Vice-principal",
@@ -164,17 +213,51 @@ work.display();
 var projects = {
 	"projects": [
 	{
-		"title": "Arcade Game",
-		"dates": 2015,
-		"description": "For this project I must recreate a classic arcade game using visual assets and a game loop engine."
+		"title": "Event Planner application",
+		"url": "https://aleksandra11.github.io/Event-Planner/#/",
+		"dates": 2016,
+		"description": "Project ",
+		//"images": [
+		//"images/",
+		//"images/"
+	//	]
+	},
+	{
+		"title": "Neighborhood Map",
+		"url": "http://aleksandra11.github.io/Project_Neighborhood_map/",
+		"dates": 2016,
+		"description": "Web Application, built using Knockout framework, Google Maps API,Google StreetView API, Fouresquare and Flickr APIs.",
+		//"images": [
+		//"images/map.png",
+		//"images/Featured_work_220.png"
+		//]
 	},
 	{
 		"title": "Portfolio",
+		"url": "http://aleksandra11.github.io/Project_Portfolio/",
 		"dates": 2015,
-		"description": "Project 'Portfolio Mockup to HTML and CSS' is responsive website with descriptions and links to each of the projects",
+		"description": "Project 'Portfolio Mockup' is responsive webpage with descriptions and links to each of the projects."
+	},
+	{
+		"title": "Arcade Game",
+		"url": "http://aleksandra11.github.io/Project_Arcade_Game/",
+		"dates": 2015,
+		"description": "An HTML5 Canvas powered arcade game developed using the best practices in Object Oriented JavaScript."
+	},
+	{
+		"title": "Feed Reader Testing",
+		"url": "http://aleksandra11.github.io/Project_Feedreader/",
+		"dates": 2015,
+		"description": "Comprehensive unit tests, using the Jasmine testing framework"
+	},
+	{
+		"title": "Web Optimization",
+		"url": "http://aleksandra11.github.io/frontend-nanodegree-mobile-portfolio/",
+		"dates": 2015,
+		"description": "Optimized an inefficient web application's JavaScript, CSS and assets delivery.",
 		"images": [
-		"images/Featured_work_220.png",
-		"images/map.png"
+		"images/map.png",
+		"images/Featured_work_220.png"
 		]
 	}
 	],
@@ -182,7 +265,12 @@ var projects = {
 		for(project in projects.projects) {
 			$("#projects").append(HTMLprojectStart);
 			var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
-			$(".project-entry:last").append(formattedTitle);
+
+			var formattedProjectUrl = HTMLprojectUrl.replace("%data%", projects.projects[project].url);
+			var formattedProjectName = formattedProjectUrl + formattedTitle;
+
+			$(".project-entry:last").append(formattedProjectName);
+
 			var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
 			$(".project-entry:last").append(formattedDates);
 			var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
